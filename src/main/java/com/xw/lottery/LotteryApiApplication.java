0af826d35c816a0a0458f210ded7733a92fbf97f
maxwell <1035071992@qq.com>
2022-09-06 22:12:54 +0800

    Initial commit

---
+++ src/main/java/com/xw/lottery/LotteryApiApplication.java
@@ -0,0 +1,13 @@
+package com.xw.lottery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LotteryApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LotteryApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xw/lottery/LotteryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xw.lottery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LotteryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
